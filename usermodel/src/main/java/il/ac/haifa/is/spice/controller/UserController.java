47fcdc7f1129d57f2114908dd40f3fff126171d6
alan <alan@10.0.0.1>
2021-01-14 22:22:33 +0200

    Minor changes

--- usermodel/src/main/java/il/ac/haifa/is/spice/controller/UserController.java
+++ usermodel/src/main/java/il/ac/haifa/is/spice/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +27,8 @@ import il.ac.haifa.is.spice.model.User;
 import il.ac.haifa.is.spice.repository.UserRepository;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
+
+// import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -46,9 +48,9 @@ public class UserController {
     }
 
     // create user rest api
-    @Operation(summary = "Create a new user")
+    @Operation(summary = "Create a new user Id and pwd should be anonimized")
     @PostMapping("/users2Create")
-    public User createUser(@RequestBody @Parameter(description = "Info of user. Id and pwd should be anonimized")  User user) {
+    public User createUser( @RequestBody  User user) {
     	
     	//user.setUserid(Anonimyzer.anonimyze(user.getUserid()));
     	//user.setPassword(Anonimyzer.anonimyze(user.getPassword()));
@@ -64,7 +66,7 @@ public class UserController {
 //    }
 //    
     // get user by userid rest api
-    @Operation(summary = "Get all the users proeprties by user name")
+    @Operation(summary = "Get all the users properties by user name")
     @GetMapping("/users2Get/{userid}")
     public ResponseEntity < User > getUserByUserid(@PathVariable String userid) {
         User user = userModelRepository.findByUserid(userid)
